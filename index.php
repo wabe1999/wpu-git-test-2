dc25366a5d099c5ede61a15c74c65d7975f179c8
Erwan Saputra <ramsissquad1999@gmail.com>
2023-06-13 11:09:22 +0800

    Memperbaiki struktur html nya

--- index.php
+++ index.php
@@ -4,13 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Git test 2</title>
-</head>
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos quo, ullam reprehenderit numquam ex culpa repellat? Ea reiciendis nostrum voluptatum amet excepturi aut quis, provident, dignissimos nam ducimus quam. Aspernatur.</p>
-<ul>
-    <li>Nama</li>
-    <li>Umur</li>
-</ul>
+    <ul>
+        <li>Nama</li>
+        <li>Umur</li>
+    </ul>
 </body>
 </html>
